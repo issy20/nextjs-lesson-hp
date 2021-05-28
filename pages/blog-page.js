a246bf5567190c846bb6ace97297c83dbbb9f91f
issy20 <gggibraltar86@gmail.com>
2021-05-28 22:44:11 +0900

    changed to function

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,21 +1,19 @@
-import Layout from "../components/Layout"
-import { getAllPostsData } from "../lib/posts"
-import Post from '../components/Post'
-const Blog = ({ posts }) => {
-  return (
-    <Layout title="Blog">
-      <ul className="m-10">
-        {posts && posts.map((post) => <Post key={post.id} post={post} />)}
-      </ul>
-    </Layout>
-  )
+import Layout from "../components/Layout";
+import { getAllPostsData } from "../lib/posts";
+import Post from "../components/Post";
+export default function Blog({ posts }) {
+	return (
+		<Layout title="Blog">
+			<ul className="m-10">
+				{posts && posts.map((post) => <Post key={post.id} post={post} />)}
+			</ul>
+		</Layout>
+	);
 }
 
-export default Blog
-
 export async function getStaticProps() {
-  const posts = await getAllPostsData();
-  return {
-    props: { posts }
-  }
+	const posts = await getAllPostsData();
+	return {
+		props: { posts },
+	};
 }
